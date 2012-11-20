ed9287c0a0d720a003308cda2f8e062bad2823d5
Andrey <insumy@gmail.com>
2012-11-20 12:30:59 +0200

    edit profile without enter current password

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if resource.update_with_password(params[resource_name])
+      set_flash_message :notice, :updated if is_navigational_format?
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords(resource)
+      respond_with_navigational(resource){ render :edit }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Bitfun::Application.routes.draw do
 
   match "/funs/tag/:tag" => "funs#index"
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users do
     member do
       get :following, :followers
