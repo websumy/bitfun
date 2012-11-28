dc2975b8beda4dfd0a4ac2c5d443d159c94eae5e
Andrey <insumy@gmail.com>
2012-11-28 14:49:34 +0200

    mounted uploader to Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,15 @@
 class Image < ActiveRecord::Base
   attr_accessible :file, :remote_file_url, :url, :tag_list
 
+  mount_uploader :file, ImageUploader
+
   # Tags
   acts_as_taggable
+
+  def attributes=(attributes = {})
+    self.url = attributes[:remote_file_url]
+    super
+  end
 
   has_one :fun, :as => :content, :dependent => :destroy
 end
