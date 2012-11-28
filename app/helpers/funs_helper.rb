f791f0d1a513c09bea9e92b7700f7ddf6b4ad3a2
Andrey <insumy@gmail.com>
2012-11-28 12:42:33 +0200

    added Fun helper show_fields

--- app/helpers/funs_helper.rb
+++ app/helpers/funs_helper.rb
@@ -1,2 +1,8 @@
 module FunsHelper
+
+  def show_fields fun
+    type = fun.content_type.downcase
+    render "funs/#{type.pluralize}/show", type.to_sym => fun.content
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126084711) do
+ActiveRecord::Schema.define(:version => 20121126084912) do
 
   create_table "funs", :force => true do |t|
     t.string   "title"
@@ -96,5 +96,10 @@ ActiveRecord::Schema.define(:version => 20121126084711) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "videos", :force => true do |t|
+    t.string "video"
+    t.string "url"
+  end
 
 end
