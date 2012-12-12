a71dd5ee881e85bca73588bbfc6f1ec6ebdedb50
Andrey <insumy@gmail.com>
2012-12-12 12:19:15 +0200

    refactoring  parse video url in Video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -13,51 +13,31 @@ class Video < ActiveRecord::Base
 
   has_many :fun, :as => :content, :dependent => :destroy
 
-  def attributes=(attributes = {})
-    ALIASES.each do |method, links|
-      links.each do |link|
-        if attributes[:url].include? link
-          self.video_type = method
-          break
-        end
-      end
-    end
-
-    self.video_id = send "parse_#{self.video_type}".to_sym, attributes[:url] if self.video_type.present?
+  def url= url
+    ALIASES.each { |k, v| v.each { |i| self.video_type = k if i.in? url } }
+    self.video_id = send "parse_#{self.video_type}", url if self.video_type.present?
     super
   end
 
   private
-
   def check_video_url
-    type = nil
-    ALIASES.each do |method, links|
-      links.each do |link|
-        if self.url.include? link
-          type = method
-          break
-        end
-      end
-    end
-
-    if type.present?
-      errors.add(:url, "can't find video from this url") if send("parse_#{type}".to_sym, self.url).nil?
+    if self.video_type.present?
+      errors.add(:url, "can't find video from this url") unless self.video_id.present?
     else
       errors.add(:url, "only Youtube.com, Vimeo.com links can be posted")
     end
-
   end
 
   def parse_youtube url
-    url.strip!
     regex = /^(?:http:\/\/)?(?:www\.)?\w*\.\w*\/(?:watch\?v=)?((?:p\/)?[\w\-]+)/
-    url.match(regex)[1] unless url.match(regex).nil?
+    matches = url.match(regex)
+    matches[1] unless matches.nil?
   end
 
   def parse_vimeo url
-    url.strip!
     regex = /^(?:http:\/\/)?(?:www\.)?vimeo\.com\/(\d+)/
-    url.match(regex)[1] unless url.match(regex).nil?
+    matches = url.match(regex)
+    matches[1] unless matches.nil?
   end
 
 end
