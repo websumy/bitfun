8b176433f3caacebe25fb1e6bf0eb370b6e46e7b
Andrey <insumy@gmail.com>
2013-01-03 11:24:57 +0200

    show_cached_tags

--- app/helpers/funs_helper.rb
+++ app/helpers/funs_helper.rb
@@ -4,4 +4,7 @@ module FunsHelper
     render "funs/#{fun.content_type.downcase.pluralize}/#{partial}", fun: fun
   end
 
+  def show_cached_tags(fun)
+    fun.content.cached_tag_list.split(", ").map { |t| link_to t, tag_path(t) }.join(', ').html_safe
+  end
 end
