4db6ce0ef93daacf8c3cd9a92d7b2594dabbf5cf
Andrey <insumy@gmail.com>
2013-01-10 12:30:00 +0200

    remove "get_string_from_query" helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,14 +14,4 @@ module ApplicationHelper
   def resource_class
     @devise_mapping.to
   end
-
-  def get_string_from_query query
-    if query.is_a? Array
-      query.join(",")
-    elsif query.is_a? String
-      query
-    else
-      query.to_s
-    end
-  end
 end
