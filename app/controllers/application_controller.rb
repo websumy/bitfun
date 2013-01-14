dfcc57cb0edff5ea1877f326eabeabdf187736c8
Andrey <insumy@gmail.com>
2013-01-14 15:54:17 +0200

    Error Handling, added 403 error template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
+  unless Rails.env.development?
+    rescue_from Exception, :with => :exception_catcher
   end
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    redirect_to root_path, alert: exception.message
+  private
+
+  def exception_catcher(exception)
+    case exception
+      when ActiveRecord::RecordNotFound, ActionController::UnknownController, ActionController::RoutingError
+        render_404(exception)
+      when CanCan::AccessDenied
+        render_403(exception)
+      else
+        render_500(exception)
+    end
+  end
+
+  def render_404(exception)
+    exception_respond_with(404)
+  end
+
+  def render_500(exception)
+    raise exception if show_detailed_exceptions?
+    exception_respond_with(500)
+  end
+
+  def render_403(exception)
+    exception_respond_with(403)
+  end
+
+  def exception_respond_with(status)
+    respond_to do |format|
+      format.html { render file: "public/#{status}", status: status, layout: false }
+      format.xml { render xml: {error: status}, status: status, layout: false }
+      format.json { render json: {error: status}, status: status, layout: false }
+    end
   end
 
   def after_sign_in_path_for(resource)
