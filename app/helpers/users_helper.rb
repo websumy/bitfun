1050b4dd939843f48772d004ba7b9bf12b87097f
Andrey <insumy@gmail.com>
2013-01-17 10:58:30 +0200

    edit helper for follow links, locales

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,18 +2,10 @@ module UsersHelper
   def follow_button user
     content_tag :div, id: "follow_form" do
       if current_user.following? user
-        form_tag unfollow_user_path, method: :delete do
-          submit_tag "Unfollow", class: "btn"
-        end
+        link_to "Unfollow", user_follow_path(user, current_user), method: :delete, class: "btn"
       else
-        form_tag follow_user_path, method: :put do
-          raw(
-              hidden_field(:user_relationship, :followed_id, value: user.id) +
-                  submit_tag("Follow", class: "btn btn-primary")
-          )
-        end
+        link_to "Follow", user_follows_path(user), method: :post, class: "btn btn-primary"
       end
     end if user_signed_in? && current_user != user
   end
-
 end
