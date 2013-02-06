bbc3783f51c3731c563a2a58b7fbfa89f2be0191
Andrey <insumy@gmail.com>
2013-02-06 12:41:54 +0200

    copy all assets from sources

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require bootstrap-dropdown
-//= require bootstrap-modal
 //= require bootstrap-tab
-//= require bootstrap-alert
 //= require rails.validations
 //= require_tree .
 
@@ -151,6 +148,210 @@ $(function(){
             }
         }
     }, "a.like");
+
+    // DROPDOWN SEARCH BLOCK
+    var dropdown_search_link = $('[data-toggle="dropdown-search"]'),
+        dropdown_search = $('.dropdown_search'),
+        dropdown_profile = $('.dropdown_profile'),
+        dropdown_profile_link = $('[data-toggle="dropdown-settings"]');
+
+    dropdown_search_link.click(function(){
+        if(dropdown_profile.is(':visible')){
+            dropdown_profile.fadeOut('fast');
+            dropdown_profile_link.parent().toggleClass('active');
+        }
+        $(this).parent().toggleClass('active');
+        dropdown_search.fadeToggle('slow');
+    });
+
+    $('.icon-close').click(function(){
+        $(this).parents('.dropdown_search').fadeOut('fast');
+        dropdown_search_link.parent().toggleClass('active');
+    });
+
+
+    // DROPDOWN SEARCH BLOCK
+    dropdown_profile_link.click(function(){
+        if(dropdown_search.is(':visible')){
+            dropdown_search.fadeOut('fast');
+            dropdown_search_link.parent().toggleClass('active');
+        }
+        $(this).parent().toggleClass('active');
+        dropdown_profile.fadeToggle('slow');
+
+
+    });
+    // FILTER OBJECT(active)
+    $('.filter_obj a').click(function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }
+        else{
+            $(this).addClass('active');
+        }
+    });
+
+    // MENU(active)
+    $('.menu a').click(function(e){
+        e.preventDefault();
+        var parent = $(this).parents('.item_wrapper');
+        parent.siblings().removeClass('active');
+        parent.addClass('active');
+    });
+
+    // ddSlick (dropdown select)
+    $('.dropdown_select').ddslick({
+    });
+
+
+    // FOLLOW/UNFOLLOW
+    $('.circle_follow_status a').bind('click', function(e){
+        e.preventDefault();
+        var status = $(this).attr('class');
+        if(status == 'follow'){
+            $(this).attr('class', 'unfollow').attr('title', 'Не хочу читать');
+        }
+        else if(status == 'unfollow'){
+            $(this).attr('class', 'follow').attr('title', 'Читать');
+        }
+    });
+
+    // SHOW/HIDE SHARE
+    $('.share_link').bind('click', function(e){
+        e.preventDefault();
+        var $$ = $(this),
+            span = $$.find('span'),
+            share_list = $$.parents('.orange_box').next('ul');
+
+        if(span.hasClass('hide')){
+            span.attr('class', 'show');
+            share_list.slideDown('fast');
+        }
+        else if(span.hasClass('show')){
+            span.attr('class', 'hide');
+            share_list.slideUp('fast');
+        }
+    });
+
+    // BOOTSTRAP
+//    $('.sidebar .nav_tabs a:first').tab('show');
+
+
+    // TOGGLE VIEW
+    $('.tumbler_switch .switch').click(function(e){
+//        if(!$(this).parents('.tumbler_switch').hasClass('toggle_reload')){
+        e.preventDefault();
+        var $$ = $(this);
+        if($$.hasClass('active')){
+            return false;
+        }
+        else{
+            var sib = $$.siblings('a'),
+                slider = sib.find('.toggle_slider'),
+                cwidth = $$.outerWidth(),
+                swidth = sib.outerWidth();
+
+            function changeClass(sib, cur){
+                cur.toggleClass('active');
+                sib.toggleClass('active');
+            }
+            //            changeClass(sib, $$);
+            if($$.hasClass('hold_left')){
+                slider.animate({left: -cwidth, width: cwidth}, 300, function(){changeClass(sib, $$);});
+            }
+            else if($$.hasClass('hold_right')){
+                slider.animate({left: swidth, width: cwidth}, 300, function(){changeClass(sib, $$);});
+            }
+
+            slider.animate({left: 0, width: swidth});
+        }
+//        }
+    });
+
+    // MASONRY JS
+    $('.main_layout.grid .post_wall').imagesLoaded( function(){
+        $('.main_layout.grid .post_wall').masonry({
+            itemSelector : '.post_card',
+            gutterWidth: 21
+        });
+    });
+    $('.feed_layout.grid .post_wall').imagesLoaded( function(){
+        $('.feed_layout.grid .post_wall').masonry({
+            itemSelector : '.post_card',
+            gutterWidth: 20
+        });
+    });
+
+    // TOOLTIPSTER
+    $('.post_layout .like_box').tooltipster({
+        interactive: true,
+        animation: 'grow',
+//        animation: 'fade',
+//        animation: 'slide',
+//        animation: 'fall',
+//        animation: 'swing',
+        theme: 'likes_theme',
+        content: '<div class="popover_content">' +
+            '<a href="#"><img src="/assets/default-avatar-30.jpg" alt="img" /></a> ' +
+            '<a href="#"><img src="/assets/default-avatar-30.jpg" alt="img" /></a> ' +
+            '<a href="#"><img src="/assets/default-avatar-30.jpg" alt="img" /></a> ' +
+            '<a href="#"><img src="/assets/default-avatar-30.jpg" alt="img" /></a> ' +
+            '<a href="#"><img src="/assets/default-avatar-30.jpg" alt="img" /></a> ' +
+            '</div>' +
+            '<div class="popover_footer">' +
+            '<a href="#">... и еще 387 людям</a>' +
+            '' +
+            '</div>'
+    });
+    $('[rel="tooltip"]').tooltipster({
+        theme: 'tooltips_theme',
+        offsetY: -5
+    });
+
+    // FANCYBOX
+    $('.modal_link').fancybox({
+        padding : 0
+    });
+    $('.signUp').fancybox({
+        wrapCSS: 'sign_form',
+        closeBtn: false,
+        padding : 0
+    });
+
+    // NOTY
+    function generate(type, text) {
+        var n = noty({
+            text: text,
+            type: type,
+            dismissQueue: true,
+            layout: 'top',
+            theme: 'defaultTheme'
+        });
+
+    }
+
+//    if($('.container').hasClass('profile_layout')){
+//        generate('information', 'К сожалению ваша учетная запись не была обновлена из-за ошибки:<br>- Адрес электронной почты<br>- Имя пользователя');
+//        generate('success', 'Ваша учетная запись была обновлена!');
+//    }
+
+    //STICKY PROFILE
+    var offset = $(".sticky_profile").offset();
+    var topPadding = 200;
+    $(window).scroll(function() {
+
+        if ($(window).scrollTop() > (offset.top  - 200)) {
+            $(".sticky_profile").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top + topPadding
+
+            }, 1000);
+        }
+        else {
+            $(".sticky_profile").stop().animate({
+                marginTop: 0
+            });
+        }
+    });
 
 });
 
---
+++ app/assets/javascripts/source/noty_position.js
@@ -0,0 +1,33 @@
+;(function($) {
+
+	$.noty.layouts.top = {
+		name: 'top',
+		options: {},
+		container: {
+			object: '<ul id="noty_top_layout_container" />',
+			selector: 'ul#noty_top_layout_container',
+			style: function() {
+				$(this).css({
+					top: 54,
+					position: 'fixed',
+					width: '100%',
+					height: 'auto',
+					margin: 0,
+					padding: 0,
+					listStyleType: 'none',
+					zIndex: 9999999
+				});
+			}
+		},
+		parent: {
+			object: '<li />',
+			selector: 'li',
+			css: {}
+		},
+		css: {
+			display: 'none'
+		},
+		addClass: ''
+	};
+
+})(jQuery);
