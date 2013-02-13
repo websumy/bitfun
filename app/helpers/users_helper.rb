28df9da5ba0a510743fbd4a5c50c108a424a5885
Andrey <insumy@gmail.com>
2013-02-13 12:57:14 +0200

    helper method -  show_avatar, castomize  follow_button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,22 @@
 module UsersHelper
   def follow_button user
-    content_tag :div, id: "follow_form" do
+   content_tag :div, class: 'circle_follow_status' do
       if current_user.following? user
-        link_to "Unfollow", user_follow_path(user, current_user), method: :delete, class: "btn"
+        link_to '', user_follow_path(user, current_user), method: :delete, class: 'unfollow', rel: 'tooltip', title: t('user.unfollow')
       else
-        link_to "Follow", user_follows_path(user), method: :post, class: "btn btn-primary"
+        link_to '', user_follows_path(user), method: :post, class: 'follow', rel: 'tooltip', title: t('user.follow')
       end
     end if user_signed_in? && current_user != user
+  end
+
+  def show_avatar(user, type = :big)
+    image_tag( user.avatar? ? user.avatar_url(:img, type) : "default-avatar-#{type}.jpg" )
   end
 
   def show_user_link
     link_to user_path(current_user), class: "item_link signUp" do
       raw "<div class='avatar_wrapper'>" +
-            image_tag( current_user.avatar? ? current_user.avatar_url(:img, :small) : 'default-avatar-32.jpg' ) +
+            show_avatar(current_user, :small) +
           "</div><span class='span_cell'><span>#{current_user.login}</span></span>"
     end
   end
