907490b60a0111cc855293de52069b5eeb1bf376
Andrey <insumy@gmail.com>
2013-02-13 18:03:19 +0200

    preview gif and video funs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -351,6 +351,33 @@ $(function(){
         }
     });
 
+    $(document).on('click', '.post_object a[data-gif]', function(e){
+        e.preventDefault();
+        $this = $(this);
+        $this.children('img').attr("src", $this.data('gif'));
+        $this.removeAttr('data-gif')
+    });
+
+    $(document).on('click', '.post_object a[data-video]', function(e){
+        e.preventDefault();
+        $this = $(this);
+        var videoUrl = {
+            youtube: 'http://www.youtube.com/embed/',
+            vimeo: 'http://player.vimeo.com/video/'
+        };
+        var videoData = $this.data('video').split('-');
+        var videoIFrame = $('<iframe />', {
+            frameborder: 0,
+            src: videoUrl[videoData[0]] + videoData[1] + '?wmode=opaque',
+            width: 500+'px',
+            height: 320+'px',
+            webkitAllowFullScreen: '',
+            mozallowfullscreen: '',
+            allowFullScreen: ''
+        });
+        $this.after(videoIFrame).remove()
+    });
+
 });
 
 function show_notice(type, text) {
