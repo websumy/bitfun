abf83dcdcfa27b27a5f5009870f4158585b0f1ab
Andrey <insumy@gmail.com>
2013-02-15 11:40:52 +0200

    only_xhr_request in likes controller

--- app/controllers/funs/likes_controller.rb
+++ app/controllers/funs/likes_controller.rb
@@ -1,32 +1,25 @@
 class Funs::LikesController < ApplicationController
   before_filter :load_fun, expect: :index
+  before_filter :only_xhr_request
 
   # Shows 10 last users who liked this fun
   def index
     users = Fun.find(params[:fun_id]).likes.limit(10).voters
-    respond_to do |format|
-      format.json { render json: users.as_json(:only => [:login, :avatar]) }
-    end
+    render json: users.as_json(:only => [:login, :avatar])
   end
 
   # Like by current_user for this fun
   def create
     authorize! :create, :like
     @fun.like_by current_user
-    respond_to do |format|
-      format.html { redirect_to @fun, notice: t("likes.like") }
-      format.json { render json: { type: :like } }
-    end
+    render json: { type: :like }
   end
 
   # Unlike by current_user for this fun
   def destroy
     authorize! :destroy, :like
     @fun.unlike_by current_user
-    respond_to do |format|
-      format.html { redirect_to @fun, notice: t("likes.like") }
-      format.json { render json: { type: :unlike } }
-    end
+    render json: { type: :unlike }
   end
 
   private
