caf3443882d19603f0f5445a2292f2aae69c4546
Andrey <insumy@gmail.com>
2013-03-21 13:00:55 +0200

    data-auth on feed and add_fun buttons

--- app/assets/javascripts/funs/add_fun.js
+++ app/assets/javascripts/funs/add_fun.js
@@ -2,7 +2,7 @@ $(function(){
 
     // Initialize fancybox for add box
 
-    $('#link_new_fun').fancybox({
+    $('#link_new_fun').not('[data-auth]').fancybox({
         type: 'ajax',
         padding : 0
     });
