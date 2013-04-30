d262fefb5082e9795aa13bb29e0dce1ee63d2876
Andrey <insumy@gmail.com>
2013-04-30 17:46:15 +0300

    modify user_helper follow links method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,29 +1,38 @@
 module UsersHelper
+  def link_to_follow(content, user, html_options)
+    status = current_user.following?(user)
+    options = { method: :post, rel: 'tooltip', title: t('user.follow'), remote: true, data: { type: :json }, style: "display: #{status ? 'none' : 'inline-block'};" }
+    link_to(content, user_follows_path(user), options.merge(html_options))
+  end
+
+  def link_to_unfollow(content, user, html_options)
+    status = current_user.following?(user)
+    options = { method: :delete, rel: 'tooltip', title: t('user.unfollow'), remote: true, data: {type: :json}, style: "display: #{status ? 'inline-block' : 'none'};" }
+    link_to(content, delete_user_follows_path(user), options.merge(html_options))
+  end
+
   def follow_button user
    content_tag :div, class: 'circle_follow_status follow_parent' do
       if current_user == user
         link_to '', edit_user_registration_path, class: 'settings', rel: 'tooltip', title: t('user.settings'), style: 'display:inline-block'
       else
-        status = current_user.following?(user)
-        link_to('', delete_user_follows_path(user), method: :delete, class: 'unfollow', rel: 'tooltip', title: t('user.unfollow'), remote: true, data: { type: :json }, style: "display: #{status ? 'inline-block' : 'none'};") +
-        link_to('', user_follows_path(user), method: :post, class: 'follow', rel: 'tooltip', title: t('user.follow'), remote: true, data: { type: :json }, style: "display: #{status ? 'none' : 'inline-block'};")
+        link_to_follow('', user, { class: 'follow' }) +
+        link_to_unfollow('', user, { class: 'unfollow' })
       end
     end if user_signed_in?
   end
 
   def follow_link user
     if user_signed_in? && current_user != user
-      status = current_user.following?(user)
-        link_to(raw('<i class="icon"></i>') + t('user.unfollow'), delete_user_follows_path(user), method: :delete, class: 'btn-follow active', remote: true, data: { type: :json }, style: "display: #{status ? 'inline-block' : 'none'};") +
-        link_to(raw('<i class="icon"></i>') + t('user.follow'), user_follows_path(user), method: :post, class: 'btn-follow', remote: true, data: { type: :json }, style: "display: #{status ? 'none' : 'inline-block'};")
+      link_to_follow(raw('<i class="icon"></i>') + t('user.follow'), user, { class: 'btn-follow' }) +
+      link_to_unfollow(raw('<i class="icon"></i>') + t('user.unfollow'), user, { class: 'btn-follow active' })
     end
   end
 
   def follow_link_rating user
     if user_signed_in? && current_user != user
-      status = current_user.following?(user)
-      link_to('', delete_user_follows_path(user), method: :delete, class: 'btn-follow unfollow', remote: true, data: { type: :json }, rel: 'tooltip', title: t('user.unfollow'), style: "display: #{status ? 'inline-block' : 'none'};") +
-      link_to('', user_follows_path(user), method: :post, class: 'btn-follow follow', remote: true, data: { type: :json }, rel: 'tooltip', title: t('user.follow'), style: "display: #{status ? 'none' : 'inline-block'};")
+      link_to_follow('', user, { class: 'btn-follow follow' }) +
+      link_to_unfollow('', user, { class: 'btn-follow unfollow' })
     end
   end
 
