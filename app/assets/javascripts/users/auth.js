e8968500788e97bb82c25feec875e2f928526fb9
Andrey <insumy@gmail.com>
2013-06-12 10:51:06 +0300

    recover password styles, js, locale

--- app/assets/javascripts/users/auth.js
+++ app/assets/javascripts/users/auth.js
@@ -12,7 +12,7 @@ $(function(){
 
     // Processing callback from
 
-    $(document).on('ajax:success', '#signup, #signin', function(evt, data, status, xhr){
+    $(document).on('ajax:success', '#signup, #signin, #recover_pass', function(evt, data, status, xhr){
         if (data.success){
             window.location = data.redirect
         }
