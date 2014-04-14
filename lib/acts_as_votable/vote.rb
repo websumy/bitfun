391aa54e589cde06e19e1f114e6481d5d4b166e0
Blaze34 <30avenyamadeo@gmail.com>
2014-04-14 11:33:48 +0300

    extend vote model

---
+++ lib/acts_as_votable/vote.rb
@@ -0,0 +1,15 @@
+module ActsAsVotable
+  class Vote < ActiveRecord::Base
+
+    after_create :create_notification
+    has_many :notifications, as: :target, dependent: :destroy
+
+    private
+
+    def create_notification
+      notifications.create(user_id: voter_id, action: :create, receiver_id: votable.user_id)
+    end
+
+  end
+
+end
