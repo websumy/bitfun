89811a392e8de17966def06bae788f76e6affcd2
Blaze34 <30avenyamadeo@gmail.com>
2014-04-15 11:19:32 +0300

    show only my notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,8 @@ class NotificationsController < ApplicationController
   authorize_resource
 
   def index
-    @notifications = Fun.unscoped { Notification.includes(:user, :target, :subject).all }
+    @notifications = Fun.unscoped do
+      Notification.includes(:user, :target, :subject).where(receiver_id: current_user.id)
+    end
   end
 end
